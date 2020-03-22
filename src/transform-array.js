787d4422e39cc212288a7336c5f57259f3dc2b41
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-03-22 12:21:13 +0300

    init: start transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,39 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform( arr ) {
+    if (!(Array.isArray(arr))) throw new Error();
+    if (arr.length == 0) return arr;
+    for (let i=0; i<arr.length; i++){
+        if(arr[i]=="--discard-next"){
+            if(i+1==arr.length){
+                arr.splice(i, 1);
+                return arr;
+            }else{
+                arr.splice(i, 2);
+                return arr;  
+            }
+        }else if(arr[i]=="--discard-prev"){
+            if(i==0){
+                arr.splice(i, 1);
+                return arr;
+            }else{
+                arr.splice(i-1, 2);
+                return arr;
+            }
+        }else if(arr[i]=="--double-next"){
+            if(i+1==arr.length){
+                arr.splice(i, 1);
+                return arr;
+            }else{
+                arr.splice(i, 1, arr[i+1]);
+                return arr;
+            }
+        }else if(arr[i]=="--double-prev"){
+            if(i==0){
+                arr.splice(i, 1);
+                return arr;
+            }else{
+                arr.splice(i, 1, arr[i-1]);
+                return arr;
+            }
+        }
+    }
+  }
