0bd81f0fdaf2a5a5f5eed4f68ad36454534a947e
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-03-22 12:15:02 +0300

    init: start count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  let cats = 0;
+  for (let i=0; i<matrix.length; i++){
+      matrix[i].forEach(item => {
+          if(item=='^^'){
+              cats++
+          }
+      })
+  }
+  return cats;
 };
